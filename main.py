b9d667c169ead1bbbc4e66eae3be9f9c99df2716
eric <eric@hphk.kr>
2021-12-12 19:25:41 +0900

    Add return types

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # uvicorn main:app --reload
 
-from typing import List
+from typing import List, Dict
 
 from fastapi import (
     FastAPI,
@@ -29,7 +29,7 @@ def get_db():
 @app.get('/movies', response_model=List[schemas.Movie])
 def read_movies(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
+) -> List[models.Movie]:
     movies = crud.get_movies(db, skip=skip, limit=limit)
     return movies
     
@@ -37,30 +37,35 @@ def read_movies(
 @app.post('/movies', response_model=schemas.Movie, status_code=201)
 def create_movie(
     movie: schemas.MovieCreate, db: Session = Depends(get_db)
-):
+) -> models.Movie:
     db_movie = crud.get_movie_by_title(db, title=movie.title)
     if db_movie:
         raise HTTPException(
             status_code=400,
             detail='Movie already exists.'
         )
-    print(movie)
     return crud.create_movie(db=db, movie=movie)
     
 
 @app.get('/movies/{movie_id}', response_model=schemas.Movie)
-def read_movie_item(movie_id: int, db: Session = Depends(get_db)):
+def read_movie_item(
+    movie_id: int, db: Session = Depends(get_db)
+) -> models.Movie:
     db_movie = crud.get_movie(db, movie_id=movie_id)
     return db_movie
-    
+
     
 @app.delete('/movies/{movie_id}', status_code=204)
-def delete_movie_item(movie_id: int, db: Session = Depends(get_db)):
+def delete_movie_item(
+    movie_id: int, db: Session = Depends(get_db)
+) -> Dict[str, str]:
     crud.delete_movie_by_id(db, movie_id=movie_id)
     return { 'message': 'successfully deleted.' }
 
 
 @app.put('/movies/{movie_id}', response_model=schemas.Movie)
-def update_movie_item(movie_id: int, movie: schemas.MovieUpdate, db: Session = Depends(get_db)):
+def update_movie_item(
+    movie_id: int, movie: schemas.MovieUpdate, db: Session = Depends(get_db)
+) -> models.Movie:
     db_movie = crud.update_movie_by_id(db, movie_id=movie_id, movie=movie)
     return db_movie
